f52e31f88ec953b318bea4ad7036b6c515a12418
Obi815 <80441506+Obi815@users.noreply.github.com>
2026-01-07 15:01:41 -0800

    espresson machine

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,18 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class ExpressoMaker {
+    constructor(make, model, color, price /*Parameters*/) {
+        this.make /*Properties*/ = make; /*Parameters*/
+        this.model /*Properties*/ = model; /*Parameters*/
+        this.color /*Properties*/ = color; /*Parameters*/
+        this.price /*Properties*/ = price; /*Parameters*/
+    }
+    brewEspresso() {
+        console.log("Brewing a fresh cup of espresso!");
+    }
+    steamMilk() {
+        console.log('Steaming milk')
+    }
+    cleanMachine() {
+        console.log('Cleaning the espresso machine')
+    }
+}
