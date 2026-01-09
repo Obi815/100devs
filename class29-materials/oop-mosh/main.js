8e3e6f0c4343c28ad78233361aaeeb421894e9dd
Obi815 <80441506+Obi815@users.noreply.github.com>
2026-01-08 19:56:50 -0800

    OOP

--- class29-materials/oop-mosh/main.js
+++ class29-materials/oop-mosh/main.js
@@ -1,1 +1,45 @@
-console.log('hello world')
+// /// Object Literals ///
+// const circle = {
+//     radius: 1, //properties
+//     location: { //properties
+//         x: 1,
+//         y: 1
+//     },
+//     draw: function () { //draw is a method
+//         console.log('draw');
+//     }
+// };
+
+// circle.draw(); //calling the method
+
+// /// Factory Functions ///
+// function createCircle(radius) {
+//     return {
+//         radius,
+//         draw: function () {
+//             console.log('dont draw');
+//         }
+//     }
+// }
+
+// const circle1 = createCircle(1);
+// circle1.draw();
+
+// /// Constructor Functions ///  
+// function NewCircle(radius) {
+//     this.radius = radius;
+//     this.draw = function () {
+//         console.log('draw from constructor');
+//     }
+// }
+
+// const circle2 = new NewCircle(1);
+
+let obj = { value: 10 }
+
+function increase(obj) {
+    obj.value++;
+}
+
+increase(obj);
+console.log(obj); //10
--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,18 +1,20 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
-class ExpressoMaker {
+class EspressoMaker {
     constructor(make, model, color, price /*Parameters*/) {
         this.make /*Properties*/ = make; /*Parameters*/
         this.model /*Properties*/ = model; /*Parameters*/
         this.color /*Properties*/ = color; /*Parameters*/
         this.price /*Properties*/ = price; /*Parameters*/
     }
-    brewEspresso() {
+    brewEspresso() { //Methods
         console.log("Brewing a fresh cup of espresso!");
     }
-    steamMilk() {
+    steamMilk() { //Methods
         console.log('Steaming milk')
     }
-    cleanMachine() {
+    cleanMachine() { //Methods
         console.log('Cleaning the espresso machine')
     }
 }
+
+let gaggia = new EspressoMaker('Gaggia', 'Classic Pro', 'Red', 400);
