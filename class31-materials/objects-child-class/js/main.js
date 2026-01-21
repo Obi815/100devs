87b02485e055d1e9c2d1115fdccf56d931b65114
Obinna Ezeka <obinnae815@gmail.com>
2026-01-20 16:18:45 -0800

    OOP Practice

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,28 +1,31 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
-class Animal{
-    constructor(name){
-        this._name = name
-    }
-    get name(){
-        return this._name
-    }
-    speak(){
-        console.log(`${this._name} makes a sound`)
-    }
-}
-class Dog extends Animal{
-    constructor(name, breed){
-        super(name)
-        this.breed = breed
-    }
-}
-class Cat extends Animal{
-    constructor(name, breed){
-        super(name)
-        this.breed = breed
-    }
-    speak(){
-        console.log('Meow')
-    }
-}
+// class Animal{
+//     constructor(name){
+//         this._name = name
+//     }
+//     get name(){
+//         return this._name
+//     }
+//     speak(){
+//         console.log(`${this._name} makes a sound`)
+//     }
+// }
+// class Dog extends Animal{
+//     constructor(name, breed){
+//         super(name)
+//         this.breed = breed
+//     }
+// }
+// class Cat extends Animal{
+//     constructor(name, breed){
+//         super(name)
+//         this.breed = breed
+//     }
+//     speak(){
+//         console.log('Meow')
+//     }
+// }
+
+
+
 
